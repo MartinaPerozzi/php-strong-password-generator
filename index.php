828b90bb0b362d008fb4412fc8f8186b98a50eda
MartinaPerozzi <perozzimartina@gmail.com>
2023-03-13 13:48:32 +0100

    init

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- TITLE -->
+	<title></title>
+	<!-- LINK -->
+	<!-- Font -->
+	<!-- serif -->
+
+	<!-- sans-serif -->
+
+	<!-- Font-awesome -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+		integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+		crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+</head>
+
+<body>
+	<div>
+        <form action="" method="GET">
+            <label for="pass-gen">Inserisci la lunghezza della password da generare:</label>
+            <input type="number" name="pass-gen" id="pass-gen" placeholder="Inserisci un numero">
+            <button type="submit">Send</button>
+        </form>
+    </div>
+
+
+</body>
+
+</html>
