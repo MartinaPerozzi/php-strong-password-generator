fd996204262f6086057d51f6f3c3a8de5a26da4a
MartinaPerozzi <perozzimartina@gmail.com>
2023-03-13 15:51:08 +0100

    + Milestone 2

---
+++ includes/function.php
@@ -0,0 +1,21 @@
+<?php 
+// Funzione che genera la password randomica
+function generatePassword ($number){
+    // Caratteri disponibili per generare la password randomica
+    $characters ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?.&%-_@#*";
+    // Creo una variabile vuota dove mettere il risultato
+    $password= null;
+
+    for ($i=0; $i <$number; $i++){
+        // Crea una password randomica da 0 alla lunghezza di $characters (tipo un indice- mi da un numero x compreso)
+        $random_pass= rand(0, strlen($characters));
+        var_dump("questo è l'indice" . $random_pass);
+        // Metti nella variabile creata in precedenza, ad ogni ciclo, un carattere randomico selezionato nella stringa (il numero x)
+        $password .= $characters[$random_pass];
+    }
+    // Returnami la password
+    return $password;
+};
+var_dump (generatePassword($number));
+
+?>
--- index.php
+++ index.php
@@ -5,24 +5,26 @@ $number = (int)$password_length;
 var_dump($number);
 
 
-function generatePassword ($number){
-    // Caratteri disponibili per generare la password randomica
-    $characters ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?.&%-_@#*";
-    // Creo una variabile vuota dove mettere il risultato
-    $password= null;
+// function generatePassword ($number){
+//     // Caratteri disponibili per generare la password randomica
+//     $characters ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?.&%-_@#*";
+//     // Creo una variabile vuota dove mettere il risultato
+//     $password= null;
 
-    for ($i=0; $i <$number; $i++){
-        // Crea una password randomica da 0 alla lunghezza di $characters (tipo un indice- mi da un numero x compreso)
-        $random_pass= rand(0, strlen($characters));
-        var_dump("questo è l'indice" . $random_pass);
-        // Metti nella variabile creata in precedenza, ad ogni ciclo, un carattere randomico selezionato nella stringa (il numero x)
-        $password .= $characters[$random_pass];
-    }
-    // Returnami la password
-    return $password;
-};
-var_dump (generatePassword($number));
+//     for ($i=0; $i <$number; $i++){
+//         // Crea una password randomica da 0 alla lunghezza di $characters (tipo un indice- mi da un numero x compreso)
+//         $random_pass= rand(0, strlen($characters));
+//         var_dump("questo è l'indice" . $random_pass);
+//         // Metti nella variabile creata in precedenza, ad ogni ciclo, un carattere randomico selezionato nella stringa (il numero x)
+//         $password .= $characters[$random_pass];
+//     }
+//     // Returnami la password
+//     return $password;
+// };
+// var_dump (generatePassword($number));
 
+// MILESTONE 2
+include (__DIR__ . '/includes/function.php');
 ?>
 
 
